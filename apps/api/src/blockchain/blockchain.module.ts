fadd24dcf981d0dc43e368f9de9a72c3da211af3
algorithmipro-creator <algorithmipro@gmail.com>
2026-04-01 11:04:14 +0300

    fix: add NotificationsModule to BlockchainModule

--- apps/api/src/blockchain/blockchain.module.ts
+++ apps/api/src/blockchain/blockchain.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlockchainService } from './blockchain.service';
 import { EvmWatcherService } from './networks/evm-watcher.service';
 import { DepositsModule } from '../deposits/deposits.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [DepositsModule],
+  imports: [DepositsModule, NotificationsModule],
   providers: [BlockchainService, EvmWatcherService],
   exports: [BlockchainService, EvmWatcherService],
 })
